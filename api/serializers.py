f27993c12c989beef263db224df7902c17532810
shereengh <shereengaa@gmail.com>
2019-10-13 22:30:55 +0300

    list API

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-
+from .models import Meal
 
 class UserCreateSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -15,3 +15,8 @@ class UserCreateSerializer(serializers.ModelSerializer):
         new_user.set_password(password)
         new_user.save()
         return validated_data
+
+class MealSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Meal
+        fields = "__all__"
--- api/views.py
+++ api/views.py
@@ -1,5 +1,17 @@
 from rest_framework.generics import CreateAPIView
-from .serializers import UserCreateSerializer
+from .serializers import MealSerializer,UserCreateSerializer
+
+from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveUpdateAPIView, DestroyAPIView, CreateAPIView
+from rest_framework.views import APIView
+
+from .models import Meal
 
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserCreateSerializer
+
+class MealList(ListAPIView):
+	serializer_class = MealSerializer
+	def get_queryset(self):
+		return Meal.objects.all()
+
