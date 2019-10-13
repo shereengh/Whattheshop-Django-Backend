5cc9b68850e904cf9ee1b3f8987a9c3b792271c1
Hessah95 <hessah.alhamadi.95@gmail.com>
2019-10-13 23:10:16 +0300

    Django Authentication

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import UserCreateAPIView
 from rest_framework_simplejwt.views import TokenObtainPairView
+################################################################
 
+# app_name="api"
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view() , name='login'),
