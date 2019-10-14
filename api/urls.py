8f9500664922a0881e51cc21064e8201154f5172
shereengh <shereengaa@gmail.com>
2019-10-14 12:27:32 +0300

    authentication and list complete working

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UserCreateAPIView
+from .views import UserCreateAPIView, MealList
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view() , name='login'),
     path('register/', UserCreateAPIView.as_view(), name='register'),
+    path('meals/list/', MealList.as_view(), name="meal-list" )
 ]
