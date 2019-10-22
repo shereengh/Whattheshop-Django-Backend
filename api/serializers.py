4bb68fffc5fdb7a217c23b0d6a6394ff73a02f92
shereengh <shereengaa@gmail.com>
2019-10-22 14:53:57 +0300

    serializer alterations

--- api/serializers.py
+++ api/serializers.py
@@ -8,12 +8,15 @@ class UserCreateSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     class Meta:
         model = User
-        fields = ['username', 'password']
+        fields = ['username', 'password', 'first_name', 'last_name', 'email']
 
     def create(self, validated_data):
         username = validated_data['username']
         password = validated_data['password']
-        new_user = User(username=username)
+        first_name = validated_data['first_name']
+        last_name = validated_data['last_name']
+        email = validated_data['email']
+        new_user = User(username=username, first_name=first_name, last_name=last_name, email=email)
         new_user.set_password(password)
         new_user.save()
         return validated_data
